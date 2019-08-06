56d6d838edcf8a047515d5e269d20d133b3c4496
Mik Myzk <myzkmik19922@gmail.com>
2019-08-07 07:38:38 +0900

    Add: related post functions

---
+++ template-parts/content-related-posts.php
@@ -0,0 +1,28 @@
+
+
+<?php if (!empty($related_posts)) { ?>
+    <div class="related-posts">
+        <hr>
+        <h3 class="widget-title"><?php _e('Related articles', 'wpcrumbs'); ?></h3>
+
+        <ul class="related-posts-list">
+            <?php
+            foreach ($related_posts as $post) {
+                setup_postdata($post);
+            ?>
+            <li>
+                <a class="title" href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
+                    <?php if (has_post_thumbnail()) { ?>
+                    <div class="thumb">
+                        <?php echo get_the_post_thumbnail(null, 'medium', array('alt' => the_title_attribute(array('echo' => false)))); ?>
+                    </div>
+                    <?php } ?>
+                    <h4><?php the_title(); ?></h4>
+                </a>
+            </li>
+            <?php } ?>
+        </ul>
+        <div class="clearfix"></div>
+    </div>
+<?php
+}
