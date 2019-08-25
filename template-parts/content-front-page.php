f1d9b8e520f37e64b64ecd7b3385ace2e2126436
miyazakimaiko <myzkmik19922@gmail.com>
2019-08-25 22:29:59 +0100

    Changed: style of the top page

--- template-parts/content-front-page.php
+++ template-parts/content-front-page.php
@@ -17,20 +17,123 @@
 
     <meta itemprop="about" content="<?php echo get_post_custom()['_aioseop_description'][0]; ?>">
     
-    <section class="article-box-in-container">
+    <div class="article-container-topPage">
     
         <div class="article-content" itemprop="articleBody">
-            <?php
-            the_content();
+            
+            <section class="flex-flow1">
 
-            wp_link_pages( array(
-                'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'colors' ),
-                'after'  => '</div>',
-            ) );
-            ?>
-        </div>
+                <div class="img-face-container">
+                    <img class="img-face" src="<?php bloginfo('template_directory'); ?>/assets/img/face.jpg" alt="Maiko Miyazaki">
+                </div>
 
-    </section>
+                <div class="topPage-wrapper1">
+                    
+                    <p>I'm a self-taught web coder and junior web developer in Dublin. I create products from scratch.</p>
+                    
+                    <div class="single-link-container">
+                        <a class="single-link1" href="https://miyazakimaiko.com/about-me">Read More About Me</a>
+                    </div>
+                
+                </div>
+
+            </section>
+
+            <section class="flex-flow2">
+
+                <div class="img-container-l-lg">
+                    <img src="<?php bloginfo('template_directory'); ?>/assets/img/desktop.png" alt="desktop" class="image-desktop">
+                </div>
+
+                <div class="topPage-wrapper2">
+                    
+                    <h3>What I do</h3>
+                    
+                    <div class="single-link-container">
+
+                        <p style="margin-bottom:10px!important">Click on a technology or service to view example projects.</p>
+
+                        <div class="tab-link-wrapper">
+
+                            <div class="tab-link-container">
+                                <a class="tab-link" href="https://miyazakimaiko.com/portfolio#port10">JavaScript</a>
+                            </div>
+
+                            <div class="tab-link-container">
+                                <a class="tab-link" href="https://miyazakimaiko.com/portfolio#port20">WordPress</a>
+                            </div>
+
+                            <div class="tab-link-container">
+                                <a class="tab-link" href="https://miyazakimaiko.com/portfolio#port30">PHP</a>
+                            </div>
+
+                            <div class="tab-link-container">
+                                <a class="tab-link" href="https://miyazakimaiko.com/portfolio#port30">HTML</a>
+                            </div>
+
+                            <div class="tab-link-container">
+                                <a class="tab-link" href="https://miyazakimaiko.com/portfolio#port10">CSS</a>
+                            </div>
+
+                            <div class="tab-link-container">
+                                <a class="tab-link" href="https://miyazakimaiko.com/portfolio#port20">Responsive Web Design</a>
+                            </div>
+
+                        </div><!-- tab-link-wrapper -->
+
+                    </div><!-- single-link-container -->
+                
+                </div><!-- topPage-wrapper1 -->
+
+            </section><!-- flex-flow2 -->
+
+            <section class="flex-flow3">
+
+                <div class="img-container-r">
+                    <a href="https://miyazakimaiko.com/portfolio">
+                        <img src="<?php bloginfo('template_directory'); ?>/assets/img/window.png" alt="window" class="image-window">
+                    </a>
+                </div>
+
+                <div class="topPage-wrapper3">
+                    
+                    <h3>My Portfolio</h3>
+                    
+                    <div class="single-link-container">
+
+                        <p>Please click the picture to see my work.</p>
+
+                    </div><!-- single-link-container -->
+
+                </div><!-- topPage-wrapper1 -->
+
+            </section><!-- flex-flow3 -->
+
+            <section class="flex-flow4">
+
+                <div class="img-container-l">
+                    <a href="https://miyazakimaiko.com/contact">
+                        <img src="<?php bloginfo('template_directory'); ?>/assets/img/mail.png" alt="mail" class="image-mail">
+                    </a>
+                </div>
+
+                <div class="topPage-wrapper4">
+                    
+                    <h3>Get in touch</h3>
+                    
+                    <div class="single-link-container">
+
+                        <p>Feel free to contact me via email!</p>
+
+                    </div><!-- single-link-container -->
+
+                </div><!-- topPage-wrapper1 -->
+
+            </section><!-- flex-flow4 -->
+        
+        </div><!-- article-content -->
+
+    </div><!-- article-box-in-container -->
 
 </article><!-- .entry-content -->
 
