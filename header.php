43ac7cddeea001d2b23869f4fe8a5b5b06f9821c
miyazakimaiko <myzkmik19922@gmail.com>
2019-09-02 00:06:59 +0100

    Fixed: bug of leyout for iphone

--- header.php
+++ header.php
@@ -14,7 +14,7 @@
 <html <?php language_attributes(); ?>>
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimum-scale=1.0, maximum-scale=1.0">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 	<link href="https://fonts.googleapis.com/css?family=Dosis:400,600&display=swap" rel="stylesheet">
 	<link href="https://fonts.googleapis.com/css?family=Lato:400,700&display=swap" rel="stylesheet">
